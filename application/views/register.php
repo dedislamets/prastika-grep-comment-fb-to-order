b932f64de906714992e8ce8551e6ff27a48fd794
dedislamets <dedi.slamets@gmail.com>
2022-02-15 13:37:35 +0700

    mumet

--- application/views/register.php
+++ application/views/register.php
@@ -183,8 +183,8 @@
                   
                   
                   <div style="border: solid 1px grey;border-radius: 10px;padding: 10px;background-color: #ececec ">
-                    LANJUT ORDER DI LIVE, KETIK #KODE_BARANG/JUMLAH_ORDER/IDMEMBER# <br>
-                    CONTOH : #001S/1/19#
+                    LANJUT ORDER DI LIVE, KETIK #KODE_BARANG/JUMLAH_ORDER/ID_MEMBER# <br>
+                    CONTOH : #001S/1/19#<br>
 
                     NOTE : WAJIB MENGGUNAKAN AWALAN DAN AKHIRAN #
                   </div>
